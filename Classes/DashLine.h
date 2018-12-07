3ee272f66cf75529cf7e9b2178641d6106be76ac
李世达 <lisd@6renyou.com>
2018-12-07 17:34:29 +0800

    重写代码

--- Classes/DashLine.h
+++ Classes/DashLine.h
@@ -6,19 +6,7 @@
 //  Copyright © 2017年 xiaomaolv. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-@interface Dashline : UIView
-{
-    NSInteger _lineLength;
-    NSInteger _lineSpacing;
-    UIColor *_lineColor;
-    CGFloat _height;
-}
-@property (assign, nonatomic)NSInteger linesLength;
-@property (assign, nonatomic) NSInteger linesSpacing;
-@property (assign, nonatomic) CGFloat heights;
-@property (strong, nonatomic) UIColor *linesColor;
 
-- (instancetype)initWithFrame:(CGRect)frame withLineLength:(NSInteger)lineLength withLineSpacing:(NSInteger)lineSpacing withLineColor:(UIColor *)lineColor;
-- (void)drawBegin;
+@interface Dashline : UIView
+
 @end
